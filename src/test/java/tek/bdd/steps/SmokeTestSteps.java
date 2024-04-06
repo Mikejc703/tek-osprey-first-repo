5baf4e62ea0b6578da4a5ad87e913b3ed8d7d650
Mike Cunningham <michael.j.cunningham10@gmail.com>
2024-04-05 22:43:40 -0400

    Message descript the change

---
+++ src/test/java/tek/bdd/steps/SmokeTestSteps.java
@@ -0,0 +1,23 @@
+package tek.bdd.steps;
+
+import io.cucumber.java.en.Then;
+import org.junit.Assert;
+import tek.bdd.pages.HomePage;
+import tek.bdd.utility.SeleniumUtilities;
+
+public class SmokeTestSteps extends SeleniumUtilities {
+
+    @Then("Validate top left corner is {string}")
+    public void validateTopLeftCorner(String expectedTitle) {
+
+        String actualTitle = getElementText(HomePage.TOP_NAV_LOGO);
+
+        Assert.assertEquals(expectedTitle, actualTitle);
+    }
+
+    @Then("Validate Sign In Button is Enabled")
+    public void validateSignInButtonIsEnabled() {
+        boolean isButtonEnabled = isElementEnabled(HomePage.SING_IN_BUTTON);
+        Assert.assertTrue(isButtonEnabled);
+    }
+}
